a0f3c48dfdcec5403365138ae034ba69bc483940
akane0303 <skycloud338a@gmail.com>
2021-05-23 00:18:34 +0900

    更新

--- controller.cpp
+++ controller.cpp
@@ -1,158 +1,127 @@
 #include"controller.h"
-#include"motor.h"
 #include<Arduino.h>
 #include<Servo.h>
-//#define DEBUG // Srial.printを有効にする時コメント化解除
+//#define DEBUG // Srial.printを有効にする時
 
 Btd btd;
-Motor mo0(PB6,PC7);
-Motor mo1(PA5,PA8);
-Motor mo2(PB10,PB4);
-Motor mo3(PB5,PB3);
-Omni omni(170,mo0,mo1,mo2,mo3);
-double theta(double x_,double y_){
-    //double x=(x_-64)*4;
-    //double y=-1*(y_-64)*4;
-    float rad=atan2(y_,x_);
-    float deg=rad/PI*180;
-    return deg;
+Motor mo0(PB4, PB10);
+Motor mo1(PB3, PB5);
+Motor mo2(PC7, PB6);
+Motor mo3(PB9, PB8);
+Omni omni(170, mo0, mo1, mo2, mo3);
+double theta(double x_, double y_) {
+  float rad = atan2(y_, x_);
+  float deg = rad / PI * 180;
+  return deg;
 }
-void setup(){
-    
-    
-    mySerial.begin(2400);
-    Serial.begin(9600);
-    
+
+void setup() {
+  mySerial.begin(2400);
+  Serial.begin(9600);
 }
-void loop(){
-    btd.Task();
-    
-    int v;
-    int x=(btd.Left_x-64)*4;
-    int y=-1*(btd.Left_y-64)*4;
-    /*
-    Serial.print("x:");
-    Serial.println(x);
-    Serial.print("y:");
-    Serial.println(y);
-    */
-    //double deg=theta(x,y);
-    //Serial.println(deg);
-   // delay(500);
-    
-    //Serial.println(theta(x,y));
-    
-    if(theta(x,y)>=60&&theta(x,y)<=120){
-       v = sqrt(x*x+y*y);        //front;
-       // Serial.println("f");
-      //  Serial.println(y);
-       
-       omni.move(0,v,0);
-        
-        //omni.move(0,y,0);
-       //Serial.print("front");  
-         
-        #ifdef DEBUG 
-        Serial.print("front");  
+void loop() {
+  btd.Task();
+  int v;
+  int x = (btd.Left_x - 64) * 4;
+  int y = -1 * (btd.Left_y - 64) * 4;
+  
+  if (theta(x, y) >= 67.5 && theta(x, y) < 112.5) {
+    v = sqrt(x * x + y * y);  //front;
+    #ifdef DEBUG
+        Serial.print("front");
         Serial.print("y :");
         Serial.println(y);
         delay(1000);
-        #endif
-        
-    }
-    else if(theta(x,y)>=-120&&theta(x,y)<=-60){
-       v = -sqrt(x*x+y*y);
-        //back;
-       omni.move(0,v,0);
-        
-        #ifdef DEBUG 
+    #endif
+  }
+  else if (theta(x, y) >= -112.5 && theta(x, y) <= -67.5) {
+    v = -sqrt(x * x + y * y);
+    //back;
+    #ifdef DEBUG
         Serial.print("y :");
         Serial.println(y);
         delay(1000);
         Serial.print("back");
-        #endif
-    }
-    else if((theta(x,y)>=-30&&theta(x,y)<0)||(theta(x,y)>0&&theta(x,y)<=30)){
-       v = sqrt(x*x+y*y);
-        //right;
-      omni.move(v,0,0);
-        /*
-         omni.move(x,0,0)
-         */
-        #ifdef DEBUG 
+    #endif
+  }
+  else if ((theta(x, y) > -22.5 && theta(x, y) < 0) || (theta(x, y) > 0 && theta(x, y) < 22.5)) {
+    v = sqrt(x * x + y * y);
+    //right;
+    #ifdef DEBUG
         Serial.print("x :");
         Serial.println(x);
-        delay(1000);
+        delay(5000);
         Serial.print("right");
-        #endif
-    }
-    
-    else if((theta(x,y)<=180&&theta(x,y)>=150)||(theta(x,y)>=-180&&theta(x,y)<=-150)){
-        //left;
-         v = -sqrt(x*x+y*y);
-         omni.move(v,0,0);
-         
-        //Serial.print("left");
-    
-        #ifdef DEBUG 
-         Serial.print("left");
+    #endif
+  }
+  else if ((theta(x, y) <= 180 && theta(x, y) > 157.5) || (theta(x, y) >= -180 && theta(x, y) < -157.5)) {
+    //left;
+    v = -sqrt(x * x + y * y);
+    #ifdef DEBUG
+        Serial.print("left");
         Serial.print("x :");
         Serial.println(x);
         delay(1000);
-        #endif
+    #endif
+  }
+  else if (theta(x, y) <= 67.5 && theta(x, y) >= 22.5) {
+    //frontRight;
+    v = sqrt(x * x + y * y);
+  }
+  else if (theta(x, y) <= 157.5 && theta(x, y) >= 112.5) {
+    //frontLeft;
+    v = sqrt(x * x + y * y);
+  }
+  else if (theta(x, y) <= -22.5 && theta(x, y) >= -67.5) {
+    //backRight;
+    v = sqrt(x * x + y * y);
+  }
+  else if (theta(x, y) <= -112.5 && theta(x, y) >= -157.5) {
+    //backLeft;
+    v = sqrt(x * x + y * y);
+  }
+  else {
+    if (btd.getButtonPress("CIRCLE")) {
     }
-    else{
-        if(btd.getButtonPress("CIRCLE")){
-            Serial.println("cir");
-        //button;
-            digitalWrite(PB6,HIGH);
-        //digitalWrite(PC7,LOW);
-        }
-        else if(btd.getButtonPress("SQUARE")){
-    
-        //button
-        }
-        else if(btd.getButtonPress("CROSS")){
-        //button
-        }
-        else if(btd.getButtonPress("TRIANGLE")){
-        //button
-        }
-    
-        else if(btd.getButtonPress("UP")){
-        //button
-        }
-    
-        else if(btd.getButtonPress("DOWN")){
-        //button
-        }
-    
-        else if(btd.getButtonPress("LEFT")){
-        //button
-        }
-
-        else if(btd.getButtonPress("RIGHT")){
-        //button
-        }
-
-        else if(btd.getButtonPress("L1")){
-        //button
-        }
-
-        else if(btd.getButtonPress("L2")){
-        //button
-        }
-
-        else if(btd.getButtonPress("R2")){
-        //button
-        }
-
-        else if(btd.getButtonPress("R1")){
-        //button
-        }
-        else {
-            omni.move(0,0,0);
-       // 
-        }
-    
+    else if (btd.getButtonPress("SQUARE")) {
+      
+      //button
     }
+    else if (btd.getButtonPress("CROSS")) {
+      //button
+    }
+    else if (btd.getButtonPress("TRIANGLE")) {
+      //button
+    }
+    else if (btd.getButtonPress("START")) {
+    }
+    else if (btd.getButtonPress("UP")) {
+    }
+    else if (btd.getButtonPress("DOWN")) {
+      //button
+    }
+    else if (btd.getButtonPress("SELECT")) {
+      //button
+    }
+    else if (btd.getButtonPress("LEFT")) {
+      //button
+    }
+    else if (btd.getButtonPress("RIGHT")) {
+    }
+    else if (btd.getButtonPress("L1")) {
+      //button
+    }
+    else if (btd.getButtonPress("L2")) {
+      //button
+    }
+    else if (btd.getButtonPress("R2")) {
+      //button
+    }
+    else if (btd.getButtonPress("R1")) {
+        
+    }
+    else {
+        //stop
+    }
+  }
+}
--- controller.h
+++ controller.h
@@ -16,28 +16,33 @@ class Btd{
         int Left_y;
         int Right_x;         
         int Right_y;
-        
         void Task();
         bool getButtonPress(String name);
         bool Check(int c,int num);
+        bool Check_SS(int c,int num0,int num1);
 };
 Btd::Btd(){
     c[0]=0x80;
-    
 }
 bool Btd::Check(int c,int num){
-    
     if((c&num)==num)return true;
     else return false;
-    
+}
+bool Btd::Check_SS(int c,int num0,int num1){
+    if((c&num0)==num0 && (c&num1)==num1)return true;
+    else return false;
+    Serial.println("b"); 
 }
 bool Btd::getButtonPress(String name){
     bool rep;
+     Serial.println(c[2]);
+     //delay(50);
+     //if(c[2]==3)Serial.println(name);
     if(name=="CROSS") rep=this->Check(c[2],0x20);
     else if(name=="CIRCLE") rep=this->Check(c[2],0x40);
     else if(name=="TRIANGLE") rep=this->Check(c[2],0x10);
     else if(name=="SQUARE") rep=this->Check(c[1],0x01);
-    else if(name=="UP") rep=this->Check(c[2],0x01);
+    else if(name=="START") rep=this->Check(c[2],0x03);
     else if(name=="DOWN") rep=this->Check(c[2],0x02);
     else if(name=="RIGHT") rep=this->Check(c[2],0x04);
     else if(name=="LEFT") rep=this->Check(c[2],0x08);
@@ -45,6 +50,8 @@ bool Btd::getButtonPress(String name){
     else if(name=="L2") rep=this->Check(c[1],0x04);
     else if(name=="R1") rep=this->Check(c[1],0x08);
     else if(name=="R2") rep=this->Check(c[1],0x10);
+    else if(name=="UP")rep=this->Check(c[2],0x01);
+    else if(name=="SELECT") rep=this->Check(c[2],0x0c);
     else rep=false; 
     return rep;
 }
@@ -56,7 +63,6 @@ void Btd::Task(){
                 if(i<7)chksum+=c[i];
             }
             //if(c[7]!=(chksum & 0x7F))throw "check sum EROOR";
-            
             String leftstickx=String(c[3],DEC);
             String leftsticky=String(c[4],DEC);
             String rightstickx=String(c[5],DEC);
@@ -65,7 +71,6 @@ void Btd::Task(){
             Left_y=leftsticky.toInt();
             Right_x=rightsticky.toInt();
             Right_y=rightsticky.toInt();
-           
             /*
             #ifdef DEBUG
                 Serial.println("left_x: "leftstickx);
